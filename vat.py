b57961ccf09e522b645984f6edfcec48d2686abf
406-04-bunprot <54165@yupparaj.ac.th>
2026-07-24 10:38:43 +0700

    Update vat.py

--- vat.py
+++ vat.py
@@ -1,9 +1,8 @@
 import streamlit as st
 st.title("🛒แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%")
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-
-net_price = price - vat
 vat = price * 0.07
+net_price = price - vat
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
 st.write("นาย บรรพรต ทรงศรี เลขที่ 4  ม.4/6")
